22d4a4de123190157144d8e4e3bf58d5721d73af
raunakkkk03 <85750313+raunakkkk03@users.noreply.github.com>
2023-01-04 23:17:08 +0530

    Update java practice.java

--- java practice.java
+++ java practice.java
@@ -91,3 +91,28 @@ find minimun value-
 }
 ouput
 1
+find second max integer
+public class Main
+{
+    public int findSecondMax(int[] arr){
+        int max=Integer.MIN_VALUE;
+        int secondMax=Integer.MIN_VALUE;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                secondMax=max;
+                max=arr[i];
+            }else if(arr[i]>secondMax && arr[i]!=max){
+                secondMax=arr[i];
+            }
+        }
+        return secondMax;
+    }
+    
+	public static void main(String[] args) {
+	    int[] arr={13,34,2,34,33,13};
+	    Main ma=new Main();
+	    System.out.println(ma.findSecondMax(arr));
+	}
+}
+ouput
+33
