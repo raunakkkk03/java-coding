db1fd9a085bf77a9b6545dcd0a8b037705b5e298
raunakkkk03 <85750313+raunakkkk03@users.noreply.github.com>
2023-01-04 21:52:15 +0530

    Update java practice.java

--- java practice.java
+++ java practice.java
@@ -38,3 +38,31 @@ public static void printArray(int[] arr){
 Output-
 	32471065
 	375
+	Reverse an array
+	public class Main
+{
+    public static void reverse(int[] numbers,int start,int end){
+        while(start<end){
+            int temp=numbers[start];
+            numbers[start]=numbers[end];
+            numbers[end]=temp;
+            start++;
+            end--;
+        }
+    }
+	 public static void printArray(int[] arr){
+           for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+     public static void main(String[] args){
+        int[] numbers={3,2,4,7,10,6,5};
+        printArray(numbers);
+        reverse(numbers,0,numbers.length-1);
+        printArray(numbers);
+    }
+}
+Output
+3,2,4,7,10,6,5
+	5,6,10,7,4,2,3
