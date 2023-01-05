d814deef29542af47819b43a7cb8889f9f415ef4
raunakkkk03 <85750313+raunakkkk03@users.noreply.github.com>
2023-01-06 00:51:30 +0530

    Update java practice.java

--- java practice.java
+++ java practice.java
@@ -151,38 +151,4 @@ public class Main
 	}
 }
 output
-public class Main
-{
-    public void printArray(int[] arr){
-        int n=arr.length;
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
-    } 
-    public void arrayDemo(){
-        int[] arr={8,1,0,2,1,0,3};
-        printArray(arr);
-        Main(arr,arr.length);
-        printArray(arr);
-    }
-    public void Main(int[] arr,int n){
-        int j=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]!=0 && arr[j]==0){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-            }
-            if(arr[j]!=0){
-                j++;
-            }
-        }
-    }
-	public static void main(String[] args) {
-		Main arrUtil=new Main();
-		arrUtil.arrayDemo();
-	}
-}
-output
 8,1,2,1,3,0,0
